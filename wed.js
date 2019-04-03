878def39c452493b0d43f486cd2f734cc4e0d016
irfanp90 <irfanp90@gmail.com>
2019-04-03 12:44:54 -0500

    update

---
+++ wed.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT:" + PORT);
+});
